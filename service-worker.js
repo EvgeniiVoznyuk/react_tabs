6b1aa441ea007e34fa71bddd30227d5ae7e53555
Evgenii <voznukzhenia@gmail.com>
2020-11-01 15:14:57 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cb55ce10f2eb2a4d10df16c6247cd130.js"
+  "./precache-manifest.f7dab4ef19f7c36b0618aa04fefa7b8c.js"
 );
 
 self.addEventListener('message', (event) => {
